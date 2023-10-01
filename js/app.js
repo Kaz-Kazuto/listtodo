1b90bcaae118563408254cd06615b9144d604fee
Kaz-Kazuto <124075265+Kaz-Kazuto@users.noreply.github.com>
2023-10-01 11:59:59 +0630

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,27 @@
+function name(params) {
+ for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+ }   
+}
+
+function hide(params) {
+    
+}
+
+function add(params) {
+    
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function show() {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if (condition) {
+
+        }
+    }
+}
